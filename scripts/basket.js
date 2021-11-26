644cde5d60ac2d7bfd67467dfbe1934775624e13
Jacek Mikrut <jacek@mikrut.art>
2021-11-26 22:03:35 +0100

    Changes in logic after review and styling

--- scripts/basket.js
+++ scripts/basket.js
@@ -5,13 +5,23 @@ class Basket {
     }
 
     clear() {
-        this.items.length = 0;
+        this.items = [];
         this.totalValue = 0;
     }
 
-    add(product) {
-        this.items.push(product);
-        this.addToTotalValue(product.price);
+    add(id, name, price) {
+        let productCount = this.check(id);
+
+        if (productCount) {
+            let count = productCount + 1;
+            this.changeCount(id, count);
+        } else {
+            let count = 1;
+            const selectedProduct = new SelectedProduct(id, name, price, count);
+            this.items.push(selectedProduct);
+        }
+
+        this.addToTotalValue(price);
     }
 
     addToTotalValue(newPrice) {
@@ -23,36 +33,42 @@ class Basket {
     }
 
     getBasketSummary() {
-        return this.items.map((product, i) => {
+        return this.items.map(product => {
             return {
-                id: i + 1,
+                id: product.id,
                 name: product.name,
                 text: `${product.count} x ${product.name} - ${product.price.toFixed(2)} zł`,
             };
         });
     }
 
-    remove(number) {
-        this.items.splice(number - 1, 1);
-    }
+    remove(id) {
+        let productCount = this.check(id);
 
-    changeCount(productName, productCount) {
-       for (let item of this.items) {
-           if (item.name === productName) item.count = productCount;
-       }
-    }
-
-    check(productName) {
-        for (let item of this.items) {
-            if (item.name === productName) return item.count;
+        if (productCount > 1) {
+            let count = productCount - 1;
+            this.changeCount(id, count);
+        } else {
+            const index = this.items.findIndex(item => item.id === id);
+            this.items.splice(index, 1);
         }
+    }
+
+    changeCount(id, count) {
+        this.items.find(item => item.id === id).count = count;
+    }
+
+    check(id) {
+        const checkProduct = this.items.find(item => item.id === id);
+        return checkProduct && checkProduct.count;
     }
 }
 
 class SelectedProduct {
-    constructor(productName, productPrice, productCount) {
-        this.name = productName;
-        this.price = productPrice;
-        this.count = productCount;
+    constructor(id, name, price, count) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.count = count;
     }
 }
